79e9d9d6242d31cae72270e2f902bcf2e5bf5dce
chainsaw <chabbershaw@gmail.com>
2025-05-28 13:51:54 -0400

    Use icon

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -21,6 +21,7 @@ import {
   getUTCDate,
 } from "./dates";
 import useLongPress from "./hooks/useLongPress";
+import { Icon } from "./components/icon";
  
 const today = new Date();
 const currentMonth = today.toLocaleString("default", {
@@ -432,25 +433,8 @@ export function TaskList() {
             [styles.recording]: isRecording,
           })}
         >
-          {isRecording ? (
-            <svg
-              height="1rem"
-              width="1rem"
-              version="1.1"
-              id="Capa_1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              viewBox="0 0 10.334 10.334"
-              xmlSpace="preserve"
-            >
-              <g>
-                <path
-                  style={{ fill: "#FFFFFF" }}
-                  d="M10.333,9.816c0,0.285-0.231,0.518-0.517,0.518H0.517C0.233,10.334,0,10.102,0,9.816V0.517
-		C0,0.232,0.231,0,0.517,0h9.299c0.285,0,0.517,0.231,0.517,0.517V9.816z"
-                />
-              </g>
-            </svg>
+          {isRecording ? (            
+            <Icon name="square" />
           ) : (
             "+"
           )}
